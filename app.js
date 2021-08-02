4cfa578e86ee1a1993af56e2629005276f090a69
sebastienbesson <sebbes06@hotmail.fr>
2021-08-02 16:26:49 +0200

    cors

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://sebastienbesson:sb100974@cluster0.7oo1s.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !')); 
 
 const app = express();
 
@@ -8,6 +16,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
