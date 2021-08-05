d4b5addd82d88354c242dc69852eee51b61170c9
sebastienbesson <sebbes06@hotmail.fr>
2021-08-05 18:24:40 +0200

    maj

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -13,17 +14,13 @@ mongoose.connect('mongodb+srv://sebastienbesson:sebastien@cluster0.7oo1s.mongodb
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors());
+
 
 app.use(bodyParser.json());
 
-app.use('/api/user', userRoutes);
-app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/sauces', sauceRoutes);
 
 
 module.exports = app;
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', sauceCtrl.createThing);
-router.get('/', sauceCtrl.getAllThings);
-router.get('/:id', sauceCtrl.getOneThing);
-router.put('/:id', sauceCtrl.modifyThing);
-router.delete('/:id', sauceCtrl.deleteThing);
+router.post('/', auth, sauceCtrl.createThing);
+router.get('/', auth, sauceCtrl.getAllThings);
+router.get('/:id', auth, sauceCtrl.getOneThing);
+router.put('/:id', auth, sauceCtrl.modifyThing);
+router.delete('/:id', auth, sauceCtrl.deleteThing);
 
 module.exports = router;
