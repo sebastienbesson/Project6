ef316454ec48c009383aa2a01908607b16df7e80
sebastienbesson <sebbes06@hotmail.fr>
2021-08-30 10:21:11 +0200

    clean images

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -32,8 +32,8 @@ exports.modifySauce = (req, res, next) => {
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Informations sur la sauce modifiées!'}))
-    .catch(error => res.status(400).json({ message: 'informations sur la sauce non modifiées' }));
+    .then(() => res.status(200).json({ message: 'Informations modifiées!'}))
+    .catch(error => res.status(403).json({ message: 'Informations non modifiées' }));
 };
 
 exports.deleteSauce = (req, res, next) => {
